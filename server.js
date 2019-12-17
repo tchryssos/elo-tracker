0b3bf558442d4a52c6740e62b8167f83a23dcc80
Troy Chryssos <troychryssos@gmail.com>
2019-12-17 09:31:47 -0500

    fixing port issue

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.use(webpackDevMiddleware(compiler, {
   },
 }));
 
-const server = app.listen(3000, function() {
+const port = process.env.PORT || 3000
+const server = app.listen(port, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Website listening at http://%s:%s', host, port);
