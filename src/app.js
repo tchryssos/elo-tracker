c092e68734f013bdcec7d4fc43ef0d0ad9d9662b
Troy Chryssos <troychryssos@gmail.com>
2020-01-17 23:30:23 -0500

    starting transition to new template

--- src/app.js
+++ src/app.js
@@ -1,12 +1,49 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom'
+import { createUseStyles } from 'react-jss'
 import Challenge from 'pages/Challenge'
+import { yellow } from 'constants/styles/colors'
+
+const useStyles = createUseStyles(() => {
+	const marPadZero = {
+		margin: 0,
+		padding: 0,
+	}
+	const baseStyle = {
+		height: '100%',
+		width: '100%',
+		fontFamily: 'roboto, sans-serif',
+		...marPadZero,
+	}
+
+	return {
+		'@import': [
+			'url("https://use.typekit.net/vot1gaz.css")',
+		],
+		'@global': {
+			html: baseStyle,
+			body: {
+				...baseStyle,
+				position: 'relative',
+				backgroundColor: yellow,
+			},
+			'#app': baseStyle,
+			div: {
+				boxSizing: 'border-box',
+			},
+			p: marPadZero,
+			h1: marPadZero,
+			h2: marPadZero,
+			h3: marPadZero,
+		},
+	}
+})
 
 const App = ({ location }) => {
 	// Create global effects or state here
 	// with access to router location
-
+	useStyles()
 	return (
 		<>
 			<Switch>
@@ -25,3 +62,4 @@ render(
 	</BrowserRouter>,
 	document.getElementById('app'),
 )
+
